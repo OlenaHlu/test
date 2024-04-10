a35e34b1314abf5d1d1ee21d46349ec8e07a568a
OlenaHlu <glushenkova_es@ukr.net>
2024-04-10 22:54:27 +0300

    new

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,26 +1,26 @@
 
-import axios from 'axios';
+// import axios from 'axios';
 
-const API_KEY = '43066959-f9f55707df0fe34b818b99119';
-const baseURL = 'https://pixabay.com/api/';
+// const API_KEY = '43066959-f9f55707df0fe34b818b99119';
+// const baseURL = 'https://pixabay.com/api/';
 
-export async function getImage(searchQuery, page, perPage) {
-  try {
-    const response = await axios.get(baseURL, {
-      params: {
-        key: API_KEY,
-        q: searchQuery,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: page,
-        per_page: perPage,
-      },
-    });
+// export async function getImage(searchQuery, page, perPage) {
+//   try {
+//     const response = await axios.get(baseURL, {
+//       params: {
+//         key: API_KEY,
+//         q: searchQuery,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//         page: page,
+//         per_page: perPage,
+//       },
+//     });
 
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    throw error;
-  }
-}
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching images:', error);
+//     throw error;
+//   }
+// }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,40 +1,40 @@
 
-import SimpleLightbox from 'simplelightbox';
+// import SimpleLightbox from 'simplelightbox';
 
-export const setGallery = document.querySelector('.gallery');
+// export const setGallery = document.querySelector('.gallery');
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-});
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+// });
 
-lightbox.refresh();
+// lightbox.refresh();
 
-export function renderImgs(images) {
+// export function renderImgs(images) {
 
-  images.forEach(image => {
-    const imgGallery = `
-      <li class="card">
-        <a href="${image.largeImageURL}" class="link">
-          <img src="${image.webformatURL}" alt="${image.tags}">
-          <ul class="list-container">
-          <li class="item-description"><h3>Likes</h3> <p>${image.likes}</p></li>
-          <li class="item-description"><h3>Views</h3> <p>${image.views}</p></li>
-          <li class="item-description"><h3>Comments</h3> <p>${image.comments}</p></li>
-          <li class="item-description"><h3>Downloads</h3> <p>${image.downloads}</p></li>
-        </ul>
-        </a>
+//   images.forEach(image => {
+//     const imgGallery = `
+//       <li class="card">
+//         <a href="${image.largeImageURL}" class="link">
+//           <img src="${image.webformatURL}" alt="${image.tags}">
+//           <ul class="list-container">
+//           <li class="item-description"><h3>Likes</h3> <p>${image.likes}</p></li>
+//           <li class="item-description"><h3>Views</h3> <p>${image.views}</p></li>
+//           <li class="item-description"><h3>Comments</h3> <p>${image.comments}</p></li>
+//           <li class="item-description"><h3>Downloads</h3> <p>${image.downloads}</p></li>
+//         </ul>
+//         </a>
         
-      </li>
-    `;
-    setGallery.insertAdjacentHTML('beforeend', imgGallery);
-  });
-  lightbox.refresh();
-}
-export function showEndOfCollectionMessage() {
-  const endMessage = document.createElement('p');
-  endMessage.classList.add('end-message');
-  endMessage.textContent =
-    "We're sorry, but you've reached the end of search results.";
-  setGallery.insertAdjacentElement('afterend', endMessage);
-}
+//       </li>
+//     `;
+//     setGallery.insertAdjacentHTML('beforeend', imgGallery);
+//   });
+//   lightbox.refresh();
+// }
+// export function showEndOfCollectionMessage() {
+//   const endMessage = document.createElement('p');
+//   endMessage.classList.add('end-message');
+//   endMessage.textContent =
+//     "We're sorry, but you've reached the end of search results.";
+//   setGallery.insertAdjacentElement('afterend', endMessage);
+// }
 
